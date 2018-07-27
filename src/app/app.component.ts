57ba3b9d1cb935ce3799cf33dc6b606e8a194d75
MOISES <mjose@todoensubastas.com.mx>
2018-07-27 09:37:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  public cities : any 
+  ngOnInit(){
+      this.getData()
+  }
+  getData(){
+    let temCities = []
+    fetch('https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json')
+    .then(data => data.json())
+    .then(cities => { 
+      cities.forEach(cities => {
+        temCities.push(cities.name)
+      });
+      this.cities = temCities
+     }).catch(error =>{
+      console.log(error);
+     })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AppComponent } from './app.component';
+import { FilterPipe} from './filter.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
